e3139238b9a0b2aaa853e8fc3d62bf25eec00d99
weerawi <weerawikramatharindu@gmail.com>
2023-05-19 21:45:01 +0530

    cardProvide is created

--- 1.food_cart/src/App.js
+++ 1.food_cart/src/App.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,14 +15,14 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {/* <h2>Let's get started!</h2> */}
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
